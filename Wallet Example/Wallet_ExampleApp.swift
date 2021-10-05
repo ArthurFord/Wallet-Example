fbd9d89a14c4c4579ec8c9a9807f48a410c516d4
Artie Ford <adf.ford@gmail.com>
2021-10-05 14:40:51 -0400

    Initial Commit

---
+++ Wallet Example/Wallet_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wallet_ExampleApp.swift
+//  Wallet Example
+//
+//  Created by Arthur Ford on 10/5/21.
+//
+
+import SwiftUI
+
+@main
+struct Wallet_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
